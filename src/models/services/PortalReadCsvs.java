310f52be8fe28a27d3dedb7b855a967ffd64a8ca
jrbigmon <juniorterriaga0@gmail.com>
2023-12-09 00:18:05 -0300

    refactor: adjusted method to read files

--- src/models/services/PortalReadCsvs.java
+++ src/models/services/PortalReadCsvs.java
@@ -1,8 +1,11 @@
 package models.services;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +14,32 @@ import models.entities.Student;
 import models.entities.Teacher;
 
 public class PortalReadCsvs implements IPortalReadCsvs {
-  public List<Student> readFileStudents(String path) {
-    List<Student> students = new ArrayList<>();
 
-    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+  public List<Student> readFileStudents(String fileName) {
+    return readFile(fileName, Student.class);
+  }
+
+  public List<Teacher> readFileTeachers(String fileName) {
+    return readFile(fileName, Teacher.class);
+  }
+
+  public List<Classroom> readFileClassrooms(String fileName) {
+    return readFile(fileName, Classroom.class);
+  }
+
+  public List<String[]> readFileTeachersToClassrooms(String fileName) {
+    return readFile(fileName, String[].class);
+  }
+
+  public List<String[]> readFileClassroomsToStudents(String fileName) {
+    return readFile(fileName, String[].class);
+  }
+
+  private <T> List<T> readFile(String fileName, Class<T> clazz) {
+    List<T> entities = new ArrayList<>();
+
+    try (BufferedReader br = new BufferedReader(
+        new InputStreamReader(getResourceAsStream(fileName), StandardCharsets.UTF_8))) {
       String line = br.readLine();
 
       int count = 0;
@@ -26,7 +51,15 @@ public class PortalReadCsvs implements IPortalReadCsvs {
             String id = fields[0].trim();
             String name = fields[1].trim();
 
-            students.add(new Student(id, name));
+            if (clazz == Student.class) {
+              entities.add(clazz.cast(new Student(id, name)));
+            } else if (clazz == Teacher.class) {
+              entities.add(clazz.cast(new Teacher(id, name)));
+            } else if (clazz == Classroom.class) {
+              entities.add(clazz.cast(new Classroom(id, name)));
+            } else if (clazz == String[].class) {
+              entities.add(clazz.cast(new String[] { id, name }));
+            }
           }
         }
 
@@ -38,127 +71,18 @@ public class PortalReadCsvs implements IPortalReadCsvs {
       System.out.println(e.getMessage());
     }
 
-    return students;
+    return entities;
   }
 
-  public List<Teacher> readFileTeachers(String path) {
-    List<Teacher> teachers = new ArrayList<>();
+  private InputStream getResourceAsStream(String fileName) {
+    InputStream inputStream = PortalReadCsvs.class.getClassLoader().getResourceAsStream(fileName);
 
-    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-      String line = br.readLine();
-
-      int count = 0;
-      while (line != null) {
-        if (count != 0) {
-          String[] fields = line.split(",");
-
-          if (fields.length == 2) {
-            String id = fields[0].trim();
-            String name = fields[1].trim();
-
-            teachers.add(new Teacher(id, name));
-          }
-        }
-
-        line = br.readLine();
-
-        count++;
-      }
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
+    if (inputStream == null) {
+      String absolutePath = new File(fileName).getAbsolutePath();
+      throw new RuntimeException("File not found in path: " + absolutePath);
     }
 
-    return teachers;
-  }
-
-  public List<Classroom> readFileClassrooms(String path) {
-    List<Classroom> classrooms = new ArrayList<>();
-
-    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-      String line = br.readLine();
-
-      int count = 0;
-      while (line != null) {
-        if (count != 0) {
-          String[] fields = line.split(",");
-
-          if (fields.length == 2) {
-            String id = fields[0].trim();
-            String name = fields[1].trim();
-
-            classrooms.add(new Classroom(id, name));
-          }
-        }
-
-        line = br.readLine();
-
-        count++;
-      }
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    }
-
-    return classrooms;
-  }
-
-  public List<String[]> readFileTeachersToClassrooms(String path) {
-    List<String[]> teachersToClassRooms = new ArrayList<>();
-
-    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-      String line = br.readLine();
-
-      int count = 0;
-      while (line != null) {
-        if (count != 0) {
-          String[] fields = line.split(",");
-
-          if (fields.length == 2) {
-            String teacherId = fields[0].trim();
-            String classroomId = fields[1].trim();
-
-            teachersToClassRooms.add(new String[] { teacherId, classroomId });
-          }
-        }
-
-        line = br.readLine();
-
-        count++;
-      }
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    }
-
-    return teachersToClassRooms;
-  }
-
-  public List<String[]> readFileClassroomsToStudents(String path) {
-    List<String[]> classroomToStudents = new ArrayList<>();
-
-    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-      String line = br.readLine();
-
-      int count = 0;
-      while (line != null) {
-        if (count != 0) {
-          String[] fields = line.split(",");
-
-          if (fields.length == 2) {
-            String classroomId = fields[0].trim();
-            String studentId = fields[1].trim();
-
-            classroomToStudents.add(new String[] { classroomId, studentId });
-          }
-        }
-
-        line = br.readLine();
-
-        count++;
-      }
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    }
-
-    return classroomToStudents;
+    return inputStream;
   }
 
 }
